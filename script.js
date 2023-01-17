b3a95c32f631a12e98bc49ab71532e1af75a246c
tbagaciunas <113136639+tbagaciunas@users.noreply.github.com>
2023-01-17 13:42:42 +0000

    init

--- script.js
+++ script.js
@@ -1,144 +1,120 @@
-// Global
-var win = window,
-    doc = document;
+LightCurtains = {};
+local = LightCurtains;
 
-// Global Functions
 
-function hasClass(el, cls) {
-  return el.className.match(new RegExp('(\\s|^)'+cls+'(\\s|$)'));
-};
+local.svg;
+local.winWidth;
+local.winHeight;
+local.totalElements;
+local.elementsArr = [];
 
-function addClass(el, cls) {
-  if (!this.hasClass(el, cls)) {
-    el.className += " " + cls;
-  }
-};
-
-function removeClass(el, cls) {
-  if (this.hasClass(el, cls)) {
-
-    var reg = new RegExp('(\\s|^)'+cls+'(\\s|$)');
-    el.className = el.className.replace(reg,' ');
-  }
-};
-
-// Elements
-
-var site = doc.getElementsByClassName('site-wrap')[0];
-var wrap = doc.getElementsByClassName('panel-wrap')[0];
-
-var panel = doc.getElementsByClassName('panel');
-
-var zoom = doc.getElementsByClassName('js-zoom');
-
-var nav_up = doc.getElementsByClassName('js-up'),
-    nav_left = doc.getElementsByClassName('js-left'),
-    nav_right = doc.getElementsByClassName('js-right'),
-    nav_down = doc.getElementsByClassName('js-down');
-
-var animation = doc.getElementsByClassName('js-animation');
-
-// Tracking
-var pos_x = 0,
-    pos_y = 0;
-
-function setPos(){
-  wrap.style.transform = 'translateX(' + pos_x + '00%) translateY(' + pos_y + '00%)';
-  setTimeout( function(){
-    removeClass(wrap, 'animate');
-  }, 600);
-}
-
-setPos();
-
-function moveUp(){
-  addClass(wrap, 'animate');
-  pos_y++;
-  setPos();
-}
-
-function moveLeft(){
-  addClass(wrap, 'animate');
-  pos_x++;
-  setPos();
-}
-
-function moveRight(){
-  addClass(wrap, 'animate');
-  pos_x--;
-  setPos();
-}
-
-function moveDown(){
-  addClass(wrap, 'animate');
-  pos_y--;
-  setPos();
-}
-
-for (var x = 0; x < nav_up.length; x++){
-  nav_up[x].addEventListener('click', moveUp);
-}
-
-for (var x = 0; x < nav_left.length; x++){
-  nav_left[x].addEventListener('click', moveLeft);
-}
-
-for (var x = 0; x < nav_right.length; x++){
-  nav_right[x].addEventListener('click', moveRight);
-}
-
-for (var x = 0; x < nav_down.length; x++){
-  nav_down[x].addEventListener('click', moveDown);
-}
-
-// Animations
-
-for (var x = 0; x < animation.length; x++){
-  ( function(_x){
-    animation[_x].addEventListener('click', function(){
-      toggleAnimation(_x);
-    });
-  })(x);
-}
-
-function toggleAnimation(i){
-  for (var x = 0; x < animation.length; x++){
-    if (i === x){
-      addClass(animation[x], 'active');
-      addClass(wrap, animation[x].getAttribute('data-animation'));
-    } else {
-      removeClass(animation[x], 'active');
-      removeClass(wrap, animation[x].getAttribute('data-animation'));
-    }
-  }
+local.init = function()
+{
   
+  $(window).on('resize',local.onResize);
+  
+  
+  local.winWidth = $(window).width();
+  local.winHeight = $(window).height();
+  local.svg = document.getElementsByTagName('svg')[0];
+  local.createGradient(local.svg,'baseGrad','linearGradient',[
+              {offset:'0%', 'stop-color':'#ffffff','stop-opacity':'0'},
+              {offset:'50%','stop-color':'#ffffff'},
+              {offset:'100%', 'stop-color':'#ffffff','stop-opacity':'0'}]);
+    
+  local.totalElements = 25;
+  local.createElements();
+};
+    
+    
+local.createElements = function()
+    {
+        var i=0;
+        var limit = local.totalElements;
+        while(i<limit)
+        { 
+          var r = 255;
+          var g = 255;
+          var b = 255;
+          var randCol = 'rgba('+r+','+g+','+b+','+(Math.random()*0.5+0.2)+')';//'#'+Math.floor(Math.random()*16777215).toString(16);
+          local.createGradient(local.svg,'grad'+i,'linearGradient',[
+              {offset:'0%', 'stop-color':randCol,'stop-opacity':'0'},
+              {offset:'10%', 'stop-color':randCol},
+              {offset:'50%','stop-color':randCol,'stop-opacity':'0'},
+              {offset:'90%', 'stop-color':randCol},
+              {offset:'100%', 'stop-color':randCol,'stop-opacity':'0'}]);
+          
+          var item = local.createRectangle(i);
+          local.elementsArr.push(item);
+          local.svg.appendChild(item); 
+          local.moveElement(item,i); 
+          ++i
+        }
+        
+    }
+    
+local.createRectangle = function(i)
+{
+    var elem = document.createElementNS("http://www.w3.org/2000/svg", 'rect');
+    elem.style.fill = 'url(#grad'+i+')';
+    elem.setAttribute("width",Math.random()*5+220);
+    elem.setAttribute("height",local.winHeight+200);
+  TweenMax.set(elem,{x:(Math.random()*10+i*30), y:-100});
+    //elem.setAttribute("x",Math.random()*250+i*20);
+    //elem.setAttribute("y",0);
+    return elem;
 }
 
-for (var x = 0; x < zoom.length; x++){
-  zoom[x].addEventListener('click', zoomOut);   
-}
-
-function zoomOut(e){
-  e.stopPropagation();
-  addClass(site, 'show-all');
-  for (var x = 0; x < panel.length; x++){
-    ( function(_x){
-      panel[_x].addEventListener('click', setPanelAndZoom);
-    })(x);
+local.createGradient = function(svg,id,type,stops){
+			 
+  var svgNS = svg.namespaceURI;
+  var grad  = document.createElementNS(svgNS,type);
+  grad.setAttribute('id',id);
+  for (var i=0;i<stops.length;i++){
+    var attrs = stops[i];
+    var stop = document.createElementNS(svgNS,'stop');
+    for (var attr in attrs){
+      if (attrs.hasOwnProperty(attr)) stop.setAttribute(attr,attrs[attr]);
+    }
+    grad.appendChild(stop);
   }
-}
-
-function setPanelAndZoom(e){
-  pos_x = -e.target.getAttribute('data-x-pos'),
-  pos_y = e.target.getAttribute('data-y-pos');
-  setPos();
-  zoomIn();
+  var defs = svg.querySelector('defs') || svg.insertBefore( document.createElementNS(svgNS,'defs'), svg.firstChild );
+  return defs.appendChild(grad); 
 }
 
 
-function zoomIn(){
-  for (var x = 0; x < panel.length; x++){
-    panel[x].removeEventListener('click', setPanelAndZoom);
-  }
-  removeClass(site, 'show-all');
+local.moveElement = function(elem,i)
+{
+  
+  
+  var xDest = (Math.random()*310+i*30);
+  var xDiff = $(elem).offset().left - xDest;
+  
+  
+  console.log(xDiff);
+  TweenMax.to(elem,Math.random()*6+1,{attr:{width:Math.random()*250+100},
+                                       x:xDest,
+                                       rotationZ:xDiff/50,
+                                       transformOrigin: "50% 20%",
+                                       //zIndex:Math.random()*local.totalElems,
+                                       ease:Quad.easeInOut,
+                                       onComplete:local.moveElement, onCompleteParams:[elem,i]})
 }
+
+
+local.onResize = function(evt)
+{
+  var i=0;
+  local.winHeight = $(window).height();
+        var limit = local.elementsArr.length;
+        while(i<limit)
+        { 
+          var elem = local.elementsArr[i];
+          elem.setAttribute('height',$(window).height()+200);
+          ++i;
+        }
+}
+
+$( document ).ready(function() {
+  local.init();
+});
