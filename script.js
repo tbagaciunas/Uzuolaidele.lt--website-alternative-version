9d959568d7419dd3d8fcba40f7538a42731c5053
tbagaciunas <113136639+tbagaciunas@users.noreply.github.com>
2023-01-17 12:24:46 +0000

    init

--- script.js
+++ script.js
@@ -1,25 +1,51 @@
-(function(){
-	$('.flex-container').waitForImages(function() {
-		$('.spinner').fadeOut();
-	}, $.noop, true);
-	
-	$(".flex-slide").each(function(){
-		$(this).hover(function(){
-			$(this).find('.flex-title').css({
-				transform: 'rotate(0deg)',
-				top: '10%'
-			});
-			$(this).find('.flex-about').css({
-				opacity: '1'
-			});
-		}, function(){
-			$(this).find('.flex-title').css({
-				transform: 'rotate(90deg)',
-				top: '15%'
-			});
-			$(this).find('.flex-about').css({
-				opacity: '0'
-			});
-		})
+$(document).ready(function() {
+
+	var curPage = 1;
+	var numOfPages = $(".skw-page").length;
+	var animTime = 1000;
+	var scrolling = false;
+	var pgPrefix = ".skw-page-";
+  
+	function pagination() {
+	  scrolling = true;
+  
+	  $(pgPrefix + curPage).removeClass("inactive").addClass("active");
+	  $(pgPrefix + (curPage - 1)).addClass("inactive");
+	  $(pgPrefix + (curPage + 1)).removeClass("active");
+  
+	  setTimeout(function() {
+		scrolling = false;
+	  }, animTime);
+	};
+  
+	function navigateUp() {
+	  if (curPage === 1) return;
+	  curPage--;
+	  pagination();
+	};
+  
+	function navigateDown() {
+	  if (curPage === numOfPages) return;
+	  curPage++;
+	  pagination();
+	};
+  
+	$(document).on("mousewheel DOMMouseScroll", function(e) {
+	  if (scrolling) return;
+	  if (e.originalEvent.wheelDelta > 0 || e.originalEvent.detail < 0) {
+		navigateUp();
+	  } else { 
+		navigateDown();
+	  }
 	});
-})();
+  
+	$(document).on("keydown", function(e) {
+	  if (scrolling) return;
+	  if (e.which === 38) {
+		navigateUp();
+	  } else if (e.which === 40) {
+		navigateDown();
+	  }
+	});
+  
+  });
