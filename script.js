239691b7a9ed4f88d0e8b608553bb9f3554a8f78
tbagaciunas <113136639+tbagaciunas@users.noreply.github.com>
2023-01-17 13:11:42 +0000

    init

--- script.js
+++ script.js
@@ -1,48 +1,151 @@
-var delayInShortMilliseconds = 350;
-var delayInMilliseconds = 500;
+(function () {
+  var expand;
+  expand = function () {
+      var $input, $search;
+      $search = $('.search');
+      $input = $('.input');
+      if ($search.hasClass('close')) {
+          $search.removeClass('close');
+          $input.removeClass('square');
+      } else {
+          $search.addClass('close');
+          $input.addClass('square');
+      }
+      if ($search.hasClass('close')) {
+          $input.focus();
+      } else {
+          $input.blur();
+      }
+  };
+  $(function () {
+      var $accordion, $wideScreen;
+      $accordion = $('#accordion').children('li');
+      $wideScreen = $(window).width() > 767;
+      if ($wideScreen) {
+          $accordion.on('mouseenter click', function (e) {
+              var $this;
+              e.stopPropagation();
+              $this = $(this);
+              if ($this.hasClass('out')) {
+                  $this.addClass('out');
+              } else {
+                  $this.addClass('out');
+                  $this.siblings().removeClass('out');
+              }
+          });
+      } else {
+          $accordion.on('touchstart touchend', function (e) {
+              var $this;
+              e.stopPropagation();
+              $this = $(this);
+              if ($this.hasClass('out')) {
+                  $this.addClass('out');
+              } else {
+                  $this.addClass('out');
+                  $this.siblings().removeClass('out');
+              }
+          });
+      }
+  });
+  $(function () {
+      var $container, $menu, $menubtn, $navbar;
+      $menubtn = $('#hb');
+      $navbar = $('.navbar');
+      $menu = $('.navigation');
+      $container = $('.site-inner');
+      $menubtn.on('click', function (e) {
+          if ($menubtn.hasClass('active')) {
+              $menubtn.removeClass('active');
+              $menu.removeClass('slide-right');
+              $container.removeClass('slide-right');
+              $navbar.removeClass('slide-right');
+          } else {
+              $menubtn.addClass('active');
+              $menu.addClass('slide-right');
+              $container.addClass('slide-right');
+              $navbar.addClass('slide-right');
+          }
+      });
+  });
+  $(function () {
+      var $button, clickOrTouch;
+      clickOrTouch = 'click touchstart';
+      $button = $('#search-button');
+      $button.on(clickOrTouch, expand);
+  });
+  $(function () {
+      var $box;
+      $box = $('.sm-box');
+      $box.on('click', function (e) {
+        e.preventDefault();
+          var $this;
+          $this = $(this);
+          if ($this.hasClass('active')) {
+              $this.removeClass('active');
+          } else {
+              $this.addClass('active');
+          }
+      });
+  });
+}.call(this));
 
-$('.navbar-button').click(function(){
-  
-  if($("#nav-icon3").hasClass('open')) {
-    $('#nav-icon3').toggleClass('open');
-    $('.menu-bc').css('animation', 'menu2 .35s ease-in-out forwards');
-    $('.menu-items').css('animation', 'menu-items2 .25s ease-in-out forwards');
-    setTimeout(function() {
-      $('.menu').css('display', 'none');
-    }, delayInShortMilliseconds);
-  } else {
-    $('#nav-icon3').toggleClass('open');
-    $('.menu-bc').css('animation', 'menu .35s ease-in-out forwards');
-    $('.menu').css('display', 'block');
-    $('.menu-items').css('animation', 'menu-items .25s .2s ease-in-out forwards');
+$("select").each(function() {
+  var $this = $(this),
+      $options = $(this).children("option").length;
+
+  $this.addClass("select-hidden");
+  $this.wrap("<div class='select'></div>");
+  $this.after("<div class='select-styled'></div>");
+
+  var $styledSelect = $this.next("div.select-styled");
+  $styledSelect.text($this.children("option").eq(0).text());
+
+  var $list = $("<ul />", {
+      "class": "select-options"
+  }).insertAfter($styledSelect);
+
+  for (var i = 0; i < $options; i++) {
+      $("<li />", {
+          text: $this.children("option").eq(i).text(),
+          rel: $this.children("option").eq(i).val()
+      }).appendTo($list);
   }
-  
+
+  var $listItems = $list.children("li");
+
+  $styledSelect.on("click", function(e) {
+      e.stopPropagation();
+      $("div.select-styled.active").each(function() {
+          $(this).removeClass("active").next("ul.select-options").hide();
+      });
+
+      $(this).toggleClass("active").next("ul.select-options").toggle();
+  });
+
+  $listItems.on("click", function(e) {
+      e.stopPropagation();
+      $styledSelect.text($(this).text()).removeClass("active");
+      $this.val($(this).attr("rel"));
+      $list.hide();
+  });
+
+  $(document).on("click", function() {
+      $styledSelect.removeClass("active");
+      $list.hide();
+  });
+
+  $(".select-sibling").next(".select-styled").css({
+    "border-top": "0px"
+  });
 });
 
-$(document).ready(function() {
-$(".button-down").click(function() {
-     $('html, body').animate({
-         scrollTop: $(".scroll1").offset().top + (0)
-     }, 750);
- });
-});
+(function () {
+var $addItem = $("#add-item");
+var $badge = $(".badge");
+var $count = 1;
 
-$('.play-but').click(function(){
-  
-  $('.video-modal-wrapper').css("display", "flex");
-  $(".video-modal-wrapper").css("animation", "fadein .3s ease-in-out forwards");
-  $(".video-modal").css("animation", "fade .3s ease-in-out forwards");
-  
-  setTimeout(function() {
-      $(".video-modal-wrapper").css("", "");
-    }, delayInMilliseconds);
+$addItem.on("click", function(e) {
+  e.preventDefault();
+  $badge.html($count++);
 });
-
-$('.video-modal-wrapper').click(function(){
-  $('.video-modal-wrapper').css("animation", "fadeout .3s ease-in-out forwards");
-  $('.video-modal').css("animation", "fade2 .3s ease-in-out forwards");
-  
-  setTimeout(function() {
-      $('.video-modal-wrapper').css("display", "none");
-    }, delayInMilliseconds);
-});
+}.call(this));
